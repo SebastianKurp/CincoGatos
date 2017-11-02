7666665dba52090069b4ba4a220c00927d62b67f
catemlitten <clitten@luc.edu>
2017-11-02 11:32:55 -0500

    Started integrating Firebase, fixed some errors relating to not have 'alt' for img tags

--- reactfrontend/src/components/AllSets.js
+++ reactfrontend/src/components/AllSets.js
@@ -9,27 +9,27 @@ class AllSets extends Component {
         <Grid fluid>
         <Row around="xs">
           <Col xs={0}/>
-          <img src={require('../images/cat.jpg')} height={150} />
-          <img src={require('../images/whale.gif')} height={150}/>
-          <img src={require('../images/walrus.gif')} height={150}/>
+          <img src={require('../images/cat.jpg')} height={150} alt={"cat"} />
+          <img src={require('../images/whale.gif')} height={150} alt={"whale"}/>
+          <img src={require('../images/walrus.gif')} height={150} alt={"walrus"}/>
           </Row>
           <Row around="xs">
           <Col xs={0} />
-          <img src={require('../images/giraffe.gif')} height={150} />
-          <img src={require('../images/owl.gif')} height={150}/>
-          <img src={require('../images/monkey.gif')} height={150}/>
-          <img src={require('../images/fox.gif')} height={150}/>
+          <img src={require('../images/giraffe.gif')} height={150} alt={"giraffe"} />
+          <img src={require('../images/owl.gif')} height={150} alt={"owl"}/>
+          <img src={require('../images/monkey.gif')} height={150} alt={"monkey"}/>
+          <img src={require('../images/fox.gif')} height={150} alt={"fox"}/>
         </Row>
         <Row around="xs">
         <Col xs={0}/>
-        <img src={require('../images/dog.gif')} height={150}/>
-        <img src={require('../images/mouse.jpg')} height={150}/>
-        <img src={require('../images/elephant.gif')} height={150}/>
+        <img src={require('../images/dog.gif')} height={150} alt={"dog"}/>
+        <img src={require('../images/mouse.jpg')} height={150} alt={"mouse"}/>
+        <img src={require('../images/elephant.gif')} height={150} alt={"elephant"}/>
         </Row>
         <Row around="xs">
         <Col xs={0}/>
-        <img src={require('../images/parrot.gif')} height={150}/>
-        <img src={require('../images/frog.gif')} height={150}/>
+        <img src={require('../images/parrot.gif')} height={150} alt={"parrot"}/>
+        <img src={require('../images/frog.gif')} height={150} alt={"frog"}/>
         </Row>
         </Grid>
       );
