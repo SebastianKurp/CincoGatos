dbb7eadfcc406b9927aa1790ad3f7bbf5c3e7814
catemlitten <clitten@luc.edu>
2017-11-02 11:59:48 -0500

    Trying to figure out Firebase Google OAuth...

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import firebase, { auth, provider } from './firebase.js';
 
 class App extends Component {
+  //Firebase
+  constructor() {
+    super();
+    this.state = {
+      user: null // not logged in == null
+    }
+}
+
+
   render() {
     return (
-        <div className="App-header">
-          <div>
-            <h2>Welcome to React</h2>
-          </div>
-          <div>
-            {this.props.children}
-          </div>
-
-        </div>
+      <div className="wrapper">
+        <h1>Cinco Gatos</h1>
+        {/*If the value of user is true they will see logout
+          if null they will be shown an option to login*/}
+        {this.state.user ?
+          <button onClick={this.logout}>Log Out</button>
+          :
+          <button onClick={this.login}>Log In</button>
+        }
+      </div>
     );
   }
 }
