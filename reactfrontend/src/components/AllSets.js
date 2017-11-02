48ee47fb68a4dd26627b68817d235d663806db1f
catemlitten <clitten@luc.edu>
2017-11-02 10:30:23 -0500

    Flexbox styling coming together

--- reactfrontend/src/components/AllSets.js
+++ reactfrontend/src/components/AllSets.js
@@ -6,14 +6,32 @@ class AllSets extends Component {
 
     render() {
       return (
-        <Row>
-          <Col xs={12} sm={3} md={2} lg={1} />
-          <img src={require('../images/cat.jpg')} height={200} />
-          <Col xs={6} sm={6} md={8} lg={10} />
-          <img src={require('../images/dog.gif')} height={200}/>
-          <Col xs={6} sm={3} md={2} lg={1} />
-          <img src={require('../images/giraffe.gif')} height={200} />
+        <Grid fluid>
+        <Row around="xs">
+          <Col xs={0}/>
+          <img src={require('../images/cat.jpg')} height={150} />
+          <img src={require('../images/whale.gif')} height={150}/>
+          <img src={require('../images/walrus.gif')} height={150}/>
+          </Row>
+          <Row around="xs">
+          <Col xs={0} />
+          <img src={require('../images/giraffe.gif')} height={150} />
+          <img src={require('../images/owl.gif')} height={150}/>
+          <img src={require('../images/monkey.gif')} height={150}/>
+          <img src={require('../images/fox.gif')} height={150}/>
         </Row>
+        <Row around="xs">
+        <Col xs={0}/>
+        <img src={require('../images/dog.gif')} height={150}/>
+        <img src={require('../images/mouse.jpg')} height={150}/>
+        <img src={require('../images/elephant.gif')} height={150}/>
+        </Row>
+        <Row around="xs">
+        <Col xs={0}/>
+        <img src={require('../images/parrot.gif')} height={150}/>
+        <img src={require('../images/frog.gif')} height={150}/>
+        </Row>
+        </Grid>
       );
     }
   }
