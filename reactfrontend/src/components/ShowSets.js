851c03589ffbdad830e4ca6b758fcdb39510bdb7
catemlitten <clitten@luc.edu>
2017-11-06 23:23:17 -0600

    Resized balls

--- reactfrontend/src/components/ShowSets.js
+++ reactfrontend/src/components/ShowSets.js
@@ -10,7 +10,7 @@ class ShowSets extends Component {
     let c = ReactDOM.findDOMNode(this.refs.myCanvas);
     let ctx = c.getContext('2d');
     var buttons, bars, mouseX = 0, mouseY = 0, keyPresses, mouseDown = false, clickBuffer = "None";
-    c.style.backgroundColor = "#000000";
+    c.style.backgroundColor = "#FFFFFF";
 
     function setup()
     {
@@ -49,21 +49,19 @@ class ShowSets extends Component {
     {
 		//Button constructor(x, y, rInactive, rActive, cFill, cStroke, wStroke, image = "None")
     	buttons = 	[
-				new Button(0.3, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
-				new Button(0.5, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
-				new Button(0.7, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
-				new Button(0.2, 0.5, 0.07, 0.085, "rgb(255,50,50)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "animals.png", "/animals"),
-				new Button(0.4, 0.5, 0.07, 0.085, "rgb(0,150,255)", "rgb(50,200,255)", "rgb(0,0,0)", 6, "None", "/household"),
-				new Button(0.6, 0.5, 0.07, 0.085, "rgb(150,35,255)", "rgb(200,85,255)", "rgb(0,0,0)", 6, "None", "/family"),
-				new Button(0.8, 0.5, 0.07, 0.085, "rgb(255,150,0)", "rgb(255,200,50)", "rgb(0,0,0)", 6, "None", "/school"),
-				new Button(0.3, 0.7, 0.07, 0.085, "rgb(0,206,209)", "rgb(50,255,255)", "rgb(0,0,0)", 6, "None", "/placeholder"),
-				new Button(0.5, 0.7, 0.07, 0.085, "rgb(255,100,125)", "rgb(255,150,175)", "rgb(0,0,0)", 6, "None", "/placeholder"),
-				new Button(0.7, 0.7, 0.07, 0.085, "rgb(255,200,0)", "rgb(255,255,50)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.3, 0.2, 0.1, 0.12, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.5, 0.2, 0.1, 0.12, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.7, 0.2, 0.1, 0.12, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.2, 0.45, 0.1, 0.12, "rgb(255,50,50)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "animals.png", "/animals"),
+				new Button(0.4, 0.45, 0.1, 0.12, "rgb(0,150,255)", "rgb(50,200,255)", "rgb(0,0,0)", 6, "None", "/household"),
+				new Button(0.6, 0.45, 0.1, 0.12, "rgb(150,35,255)", "rgb(200,85,255)", "rgb(0,0,0)", 6, "None", "/family"),
+				new Button(0.8, 0.45, 0.1, 0.12, "rgb(255,150,0)", "rgb(255,200,50)", "rgb(0,0,0)", 6, "None", "/school"),
+				new Button(0.3, 0.7, 0.1, 0.12, "rgb(0,206,209)", "rgb(50,255,255)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.5, 0.7, 0.1, 0.12, "rgb(255,100,125)", "rgb(255,150,175)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.7, 0.7, 0.1, 0.12, "rgb(255,200,0)", "rgb(255,255,50)", "rgb(0,0,0)", 6, "None", "/placeholder"),
     				];
 			//Bar constructor(x1, y1, x2, y2, cActive, cInactive, cStroke, wStroke)
     	bars =	[
-    				new Bar(0.05, 0.05, 0.65, 0.1, "rgb(100,100,100)", "rgb(50,50,50)", "rgb(0,0,0)", 6),
-    				new Bar(0.70, 0.05, 0.95, 0.1, "rgb(255,255,255)", "rgb(200,200,200)", "rgb(0,0,0)", 6),
     				new Bar(0.50, 0.85, 0.70, 0.95, "rgb(255,255,255)", "rgb(200,200,200)", "rgb(0,0,0)", 6),
     				new Bar(0.75, 0.85, 0.95, 0.95, "rgb(255,255,255)", "rgb(200,200,200)", "rgb(0,0,0)", 6)
     			]
@@ -72,7 +70,7 @@ class ShowSets extends Component {
     function move()
     {
 			ctx.canvas.width = window.innerWidth;
-			ctx.canvas.height = window.innerHeight;
+			ctx.canvas.height = window.innerHeight - 100;
 			ctx.clearRect(0, 0, c.width, c.height);
 	
 			ctx.beginPath();
