24cad0fdd1336d16cdcc04c20ae814e4d307880c
catemlitten <clitten@luc.edu>
2017-11-06 20:38:02 -0600

    Adding in Abdul's changes

--- reactfrontend/src/components/Button.js
+++ reactfrontend/src/components/Button.js
@@ -2,18 +2,22 @@ import { Component } from 'react'
 
 class Button extends Component
 {
-	constructor(x, y, rInactive, rActive, cFill, cStroke, wStroke, image = "None")
+	
+	constructor(x, y, rInactive, rActive, cFill, cActive, cStroke, wStroke, image = "None", link = "None")
 	{
     super(); //added this to Abdul's code b/c of error
-		this.x = x;
-		this.y = y;
-		this.rInactive = rInactive;
-		this.rActive = rActive;
-		this.rCurrent = rInactive;
-		this.cFill = cFill;
-		this.cStroke = cStroke;
-		this.wStroke = wStroke;
-		this.image = image;
+	this.x = x;
+	this.y = y;
+	this.rInactive = rInactive;
+	this.rActive = rActive;
+	this.rCurrent = rInactive;
+	this.cFill = cFill;
+	this.cActive = cActive;
+	this.cCurrent = cFill;
+	this.cStroke = cStroke;
+	this.wStroke = wStroke;
+	this.image = image;
+	this.link = link;
 	}
 
 	draw(ctx, canvasWidth, canvasHeight)
@@ -24,7 +28,7 @@ class Button extends Component
 		var r = this.rCurrent * Math.min(canvasWidth, canvasHeight)
 		ctx.arc(this.x * canvasWidth,this.y * canvasHeight,r,0,2*Math.PI);
 		ctx.stroke();
-		ctx.fillStyle = this.cFill;
+		ctx.fillStyle = this.cCurrent;
 		ctx.fill();
 		if (this.image !== "None")
 		{
@@ -35,12 +39,33 @@ class Button extends Component
 	  	}
 	}
 
-	size(mouseX, mouseY, canvasWidth, canvasHeight)
+	isTouchingMouse(mouseX, mouseY, canvasWidth, canvasHeight)
 	{
 		var r = this.rCurrent * Math.min(canvasWidth, canvasHeight)
 		if (Math.sqrt(Math.pow(this.x * canvasWidth - mouseX, 2) + Math.pow(this.y * canvasHeight - mouseY, 2)) < r + this.wStroke / 2)
+			return true;
+		else return false;
+	}
+
+	size(mouseX, mouseY, canvasWidth, canvasHeight)
+	{
+		if (this.isTouchingMouse(mouseX, mouseY, canvasWidth, canvasHeight))
 			this.rCurrent = (this.rCurrent * 9 + this.rActive) / 10;
 		else this.rCurrent = (this.rCurrent * 9 + this.rInactive) / 10;
+	}
+
+	isClicked(isMouseDown, mouseX, mouseY, canvasWidth, canvasHeight)
+	{
+		if (this.isTouchingMouse(mouseX, mouseY, canvasWidth, canvasHeight) && isMouseDown)
+		{
+			this.cCurrent = this.cActive;
+			return true;
+		}
+		else 
+		{
+			this.cCurrent = this.cFill;
+			return false;
+		}
 	}
 }
 
--- reactfrontend/src/components/ShowSets.js
+++ reactfrontend/src/components/ShowSets.js
@@ -9,7 +9,7 @@ class ShowSets extends Component {
   componentDidMount(){
     let c = ReactDOM.findDOMNode(this.refs.myCanvas);
     let ctx = c.getContext('2d');
-    var buttons, bars, mouseX = 0, mouseY = 0, keyPresses, mouseDown = false;
+    var buttons, bars, mouseX = 0, mouseY = 0, keyPresses, mouseDown = false, clickBuffer = "None";
     c.style.backgroundColor = "#FFFFFF";
 
     function setup()
@@ -19,7 +19,10 @@ class ShowSets extends Component {
     	{
     		keyPresses.push(false)
     	}
-    	initialize();
+			initialize();
+			//create buttons and bars
+
+
     	setInterval(move, 1000.0/60.0);
       //interval var was causing issue because it was never called
     	document.body.addEventListener("mousemove", function(e) {
@@ -33,30 +36,31 @@ class ShowSets extends Component {
     		mouseDown = false;
     	});
     	document.body.addEventListener("keydown", function(e) {
-            keyPresses[e.keyCode] = true;
-        });
-        document.body.addEventListener("keyup", function(e) {
-            keyPresses[e.keyCode] = false;
-        });
+        keyPresses[e.keyCode] = true;
+      });
+      document.body.addEventListener("keyup", function(e) {
+        keyPresses[e.keyCode] = false;
+      });
 
     }
 
 
     function initialize()
     {
+		//Button constructor(x, y, rInactive, rActive, cFill, cStroke, wStroke, image = "None")
     	buttons = 	[
-    					new Button(0.3, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(0,0,0)", 6),
-    					new Button(0.5, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(0,0,0)", 6),
-    					new Button(0.7, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(0,0,0)", 6),
-    					new Button(0.2, 0.5, 0.07, 0.085, "rgb(255,50,50)", "rgb(0,0,0)", 6, "animals.png"),
-    					new Button(0.4, 0.5, 0.07, 0.085, "rgb(0,150,255)", "rgb(0,0,0)", 6),
-    					new Button(0.6, 0.5, 0.07, 0.085, "rgb(150,35,255)", "rgb(0,0,0)", 6),
-    					new Button(0.8, 0.5, 0.07, 0.085, "rgb(255,150,0)", "rgb(0,0,0)", 6),
-    					new Button(0.3, 0.7, 0.07, 0.085, "rgb(0,206,209)", "rgb(0,0,0)", 6),
-    					new Button(0.5, 0.7, 0.07, 0.085, "rgb(255,100,125)", "rgb(0,0,0)", 6),
-    					new Button(0.7, 0.7, 0.07, 0.085, "rgb(255,200,0)", "rgb(0,0,0)", 6),
+				new Button(0.3, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.5, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.7, 0.3, 0.07, 0.085, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.2, 0.5, 0.07, 0.085, "rgb(255,50,50)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "animals.png", "/animals"),
+				new Button(0.4, 0.5, 0.07, 0.085, "rgb(0,150,255)", "rgb(50,200,255)", "rgb(0,0,0)", 6, "None", "/household"),
+				new Button(0.6, 0.5, 0.07, 0.085, "rgb(150,35,255)", "rgb(200,85,255)", "rgb(0,0,0)", 6, "None", "/family"),
+				new Button(0.8, 0.5, 0.07, 0.085, "rgb(255,150,0)", "rgb(255,200,50)", "rgb(0,0,0)", 6, "None", "/school"),
+				new Button(0.3, 0.7, 0.07, 0.085, "rgb(0,206,209)", "rgb(50,255,255)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.5, 0.7, 0.07, 0.085, "rgb(255,100,125)", "rgb(255,150,175)", "rgb(0,0,0)", 6, "None", "/placeholder"),
+				new Button(0.7, 0.7, 0.07, 0.085, "rgb(255,200,0)", "rgb(255,255,50)", "rgb(0,0,0)", 6, "None", "/placeholder"),
     				];
-
+			//Bar constructor(x1, y1, x2, y2, cActive, cInactive, cStroke, wStroke)
     	bars =	[
     				new Bar(0.05, 0.05, 0.65, 0.1, "rgb(100,100,100)", "rgb(50,50,50)", "rgb(0,0,0)", 6),
     				new Bar(0.70, 0.05, 0.95, 0.1, "rgb(255,255,255)", "rgb(200,200,200)", "rgb(0,0,0)", 6),
@@ -67,25 +71,34 @@ class ShowSets extends Component {
 
     function move()
     {
-      	ctx.canvas.width = window.innerWidth - 200; //sets size of canvas width + height in window
-      	ctx.canvas.height = window.innerHeight - 80;
-      	ctx.clearRect(0, 0, c.width, c.height);
-
-      ctx.beginPath();
-    	ctx.arc(c.width/2, c.height/2, Math.min(c.width, c.height)/2, 0, 2*Math.PI);
-    	ctx.fillStyle = "rgb(200,255,255)"
-    	ctx.fill();
-
-    	for (var k = 0; k < buttons.length; k++)
-    	{
-    		buttons[k].size(mouseX - 90, mouseY - 70, c.width, c.height); //both were - 10
-    		buttons[k].draw(ctx, c.width, c.height);
-    	}
-    	for (var i = 0; i < bars.length; i++)
-    	{
-    		bars[i].color(mouseX - 50, mouseY - 50, c.width, c.height) //had no subtraction
-    		bars[i].draw(ctx, c.width, c.height);
-    	}
+			ctx.canvas.width = window.innerWidth - 125;
+			ctx.canvas.height = window.innerHeight - 100;
+			ctx.clearRect(0, 0, c.width, c.height);
+	
+			ctx.beginPath();
+			ctx.arc(c.width/2, c.height/2, Math.min(c.width, c.height)/2, 0, 2*Math.PI);
+			ctx.fillStyle = "rgb(200,255,255)"
+			ctx.fill();
+			
+			for (var j = 0; j < buttons.length; j++)
+			{
+				buttons[j].size(mouseX - 90, mouseY - 70, c.width, c.height);
+				if (buttons[j].isClicked(mouseDown, mouseX - 90, mouseY - 70, c.width, c.height))
+				{
+					clickBuffer = buttons[j].link;
+				}
+				if (!mouseDown && clickBuffer !== "None")
+				{
+					console.log(clickBuffer);
+					clickBuffer = "None"
+				}
+				buttons[j].draw(ctx, c.width, c.height);
+			}
+			for (var i = 0; i < bars.length; i++)
+			{
+				bars[i].color(mouseX-90, mouseY-70, c.width, c.height)
+				bars[i].draw(ctx, c.width, c.height);
+			}
     }
 
 
