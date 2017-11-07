3cd2aa1700fa223b1829411544d9311b6e58e0d4
catemlitten <clitten@luc.edu>
2017-11-06 22:25:11 -0600

    Set up initial canvas in Animals for Abdul to style.

--- reactfrontend/src/components/Animals.js
+++ reactfrontend/src/components/Animals.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Button from './Button'
+import Bar from './Bar'
 
 class Animals extends Component {
+
+      componentDidMount(){
+    let c = ReactDOM.findDOMNode(this.refs.myCanvas);
+    let ctx = c.getContext('2d');
+    var buttons, bars, mouseX = 0, mouseY = 0, keyPresses, mouseDown = false, clickBuffer = "None";
+    c.style.backgroundColor = "#000000";
+
+  }
+
+
 
     render() {
       return (
         <div id='body'>
-        <Grid fluid>
-        <Row>
-          <Col xs={12}>
-            <Row center="xs">
-            Animals
-              <Col xs={6} />
-            </Row>
-          </Col>
-        </Row>
-        </Grid>
+       <canvas ref="myCanvas" />
       </div>
       );
     }
