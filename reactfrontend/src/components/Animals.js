c5fe55414466675b4e85f21d387e8b32d211bfeb
abdulzakkar <azakkar@luc.edu>
2017-11-12 21:26:54 -0600

    refactored code for buttons and completed working on them, aside from image rendering on circle buttons. Also cleaned up layout and added text.

--- reactfrontend/src/components/Animals.js
+++ reactfrontend/src/components/Animals.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Card from './Card'
-import Button from './Button'
+import CircleButton from './CircleButton'
 import Bar from './Bar'
 import { Link, Redirect, withRouter } from 'react-router-dom'
 
@@ -44,49 +44,22 @@ class Animals extends Component {
     function initialize()
     {
       var color = [255,255,255];
-      card = new Card(0.5, 0.3, 0.3, 0.2, 50, color, "QUESTION");
+      card = new Card(0.5, 0.3, 0.3, 0.2, 50, color, "Question?");
+      //constructor(x, y, rx, ry, cFill, cActive, cStroke, wStroke, link = "None", text = "")
       bars =  [
-            new Bar(0.2, 0.6, 0.8, 0.675, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6),
-            new Bar(0.2, 0.7, 0.8, 0.775, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6),
-            new Bar(0.2, 0.8, 0.8, 0.875, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6),
-            new Bar(0.2, 0.9, 0.8, 0.975, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6)
+            new Bar(0.5, 0.65, 0.3, 0.0375, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "None", "Option 1"),
+            new Bar(0.5, 0.75, 0.3, 0.0375, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "None", "Option 2"),
+            new Bar(0.5, 0.85, 0.3, 0.0375, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "None", "Option 3"),
+            new Bar(0.5, 0.95, 0.3, 0.0375, "rgb(255,150,150)", "rgb(255,100,100)", "rgb(0,0,0)", 6, "None", "Option 4")
       ]
+      //constructor(x, y, rInactive, rActive, cFill, cActive, cStroke, wStroke, image = "None", link = "None", text = "")
       buttons = [
-        new Button(0.9, 0.9, 0.07, 0.09, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6)
+        new CircleButton(0.9, 0.9, 0.07, 0.09, "rgb(100,100,100)", "rgb(150,150,150)", "rgb(0,0,0)", 6, "None", "None", "Next")
       ]
     }
 
-    function move()
+    function drawButtons(buttons)
     {
-      ctx.canvas.width = window.innerWidth;
-      ctx.canvas.height = window.innerHeight - 100;
-      ctx.clearRect(0, 0, c.width, c.height);
-      if (keyPresses[32])
-      {
-        if (!card.isFlipping)
-        {
-          startFlipFrame = frame;
-          if (otherColor[0] == 200 && otherColor[1] == 0 && otherColor[2] == 0) 
-          {
-            otherColor = [255,255,255];
-            card.text = "QUESTION";
-          }
-          else 
-          {
-            otherColor = [200,0,0];
-            card.text = "ANSWER";
-          }
-          card.drawFlip(ctx, frame - startFlipFrame, otherColor, c.width, c.height);
-        }
-      }
-      if (card.isFlipping) card.drawFlip(ctx, frame - startFlipFrame, otherColor, c.width, c.height);
-      else card.draw(ctx, c.width, c.height);
-
-      for (var i = 0; i < bars.length; i++)
-      {
-        bars[i].color(mouseX, mouseY - 75, c.width, c.height)
-        bars[i].draw(ctx, c.width, c.height);
-      }
       for (var j = 0; j < buttons.length; j++)
       {
         buttons[j].size(mouseX, mouseY - 75, c.width, c.height);
@@ -104,6 +77,46 @@ class Animals extends Component {
         }
         buttons[j].draw(ctx, c.width, c.height);
       }
+    }
+
+    function drawBars(bars)
+    {
+      for (var i = 0; i < bars.length; i++)
+      {
+        bars[i].isTouchingMouse(mouseX, mouseY - 75, c.width, c.height)
+        bars[i].draw(ctx, c.width, c.height);
+      }
+    }
+
+    function move()
+    {
+      ctx.canvas.width = window.innerWidth;
+      ctx.canvas.height = window.innerHeight - 100;
+      ctx.clearRect(0, 0, c.width, c.height);
+
+      drawButtons(buttons);
+      drawBars(bars);
+
+      if (keyPresses[32])
+      {
+        if (!card.isFlipping)
+        {
+          startFlipFrame = frame;
+          if (otherColor[0] === 200 && otherColor[1] === 0 && otherColor[2] === 0) 
+          {
+            otherColor = [255,255,255];
+            card.text = "Question?";
+          }
+          else 
+          {
+            otherColor = [200,0,0];
+            card.text = "Answer!";
+          }
+          card.drawFlip(ctx, frame - startFlipFrame, otherColor, c.width, c.height);
+        }
+      }
+      if (card.isFlipping) card.drawFlip(ctx, frame - startFlipFrame, otherColor, c.width, c.height);
+      else card.draw(ctx, c.width, c.height);
 
       frame++;
     }
