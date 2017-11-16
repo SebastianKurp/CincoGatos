2e77b38872d20a8c31dbf021176bbad4611e72f8
Paul Narup <pnarup@luc.edu>
2017-11-16 12:36:19 -0600

    landing page

--- reactfrontend/src/components/Home.js
+++ reactfrontend/src/components/Home.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import './styles/landing.css'
 
 class Animals extends Component {
 
     render() {
       return (
-        <div id='body'>
-        <Grid fluid>
-        <Row>
-          <Col xs={12}>
-            <Row center="xs">
-            Home page
-              <Col xs={6} />
-            </Row>
-          </Col>
-        </Row>
-        </Grid>
-      </div>
+        <div id="body">
+          <div id="logo">
+            <p id="logo-name">Cinco Gatos</p>
+          </div>
+        </div>
       );
     }
   }
