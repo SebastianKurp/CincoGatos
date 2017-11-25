b425ef0d0153f143507b4deb2a9aa6e69436ee6d
catemlitten <clitten@luc.edu>
2017-11-25 09:40:30 -0600

    Could not transistion cards -- fixed.

--- CG_ThanksgivingRedo/public/js/bundle.js
+++ CG_ThanksgivingRedo/public/js/bundle.js
@@ -25140,7 +25140,6 @@ async function initialize()
 
 function drawButtons()
 {
-  console.log("drawButtons is called");
   for (var i = 0; i < buttons.length; i++)
   {
     buttons[i].size(mouseX, mouseY - 75, c.width, c.height);
@@ -25156,7 +25155,6 @@ function drawButtons()
 
 function drawBars(options)
 {
-  console.log("drawBars is callled");
   for (var i = 0; i < bars.length; i++)
   {
     bars[i].text = options[i];
@@ -25174,9 +25172,8 @@ function drawBars(options)
   }
 }
 
-function move()
+async function move()
 {
-  console.log("move is called")
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight - 100;
   ctx.clearRect(0, 0, c.width, c.height);
@@ -25207,7 +25204,7 @@ function move()
   {
     complete = false;
     clickedNext = false;
-    userfunctions.setCards();
+    await setCards();
     card.text = currentQuestion;
     startFlipFrame = frame;
     color = [255,255,255]
