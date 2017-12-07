f3497b243625938d3f647355583b888fd42bfaa3
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-12-07 13:24:06 -0600

    Added more to auth.js

--- CG_ThanksgivingRedo/public/js/auth.js
+++ CG_ThanksgivingRedo/public/js/auth.js
@@ -1,5 +1,5 @@
 module.exports.autherrors = auth 
-
+//todo, Nieky will probably check this so do it...
 function auth(errors){
 
   switch(error.code){
@@ -8,6 +8,14 @@ function auth(errors){
     req.flash('message','Username Not Found');
     res.redirect('/login');
       }
+    case 'auth/wrong-password':{
+        console.log('Wrong Password');
+        req.flash('message','Wrong Password');
+        res.redirect('/login');
+    }
+    default: {
+      return error.message;
+  }
     }
   }
 
