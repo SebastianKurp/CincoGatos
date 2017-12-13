64e916d14bdeb88bb58f05abaf9e967b1d3853bc
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-12-13 10:08:32 -0600

    Trying to get travis to work

--- Main_Project/test/unauth.test.js
+++ Main_Project/test/unauth.test.js
@@ -1,23 +1,23 @@
-// import should from 'should'
-// import { getFirebase } from './firebase'
+import should from 'should'
+import { getFirebase } from './firebase'
 
-// describe('Unauthenticated firebase', () => {
-//   let fb
+describe('Unauthenticated firebase', () => {
+  let fb
 
-//   before(() => {
-//     fb = getFirebase(undefined, 'customName')
-//   })
+  before(() => {
+    fb = getFirebase(undefined, 'customName')
+  })
 
-//   it('does not allow fetch user', (done) => {
-//     fb
-//       .then((firebase) => {
-//         firebase.database().ref('user').once('value')
-//           .then((sn) => {
-//             done(new Error('should not fetch user'))
-//           })
-//           .catch((error) => {
-//             done()
-//           })
-//       })
-//   })
-// })
+  it('does not allow fetch user', (done) => {
+    fb
+      .then((firebase) => {
+        firebase.database().ref('user').once('value')
+          .then((sn) => {
+            done(new Error('should not fetch user'))
+          })
+          .catch((error) => {
+            done()
+          })
+      })
+  })
+})
