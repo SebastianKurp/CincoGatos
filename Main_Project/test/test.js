7813f9710941cea5b267b219cb79d0736de8a4c3
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-12-13 21:20:43 -0600

    Updated the last firebase tests.

--- Main_Project/test/test.js
+++ Main_Project/test/test.js
@@ -1,3 +1,12 @@
+//Source for test #1
+var users = {
+  ref: function () {
+    return firebase.database().ref();
+  }
+  create: function (credentials, callback) {
+    users.ref().createUser(credentials, callback);
+  }
+};
 //Test for creating a user
 MockFirebase.override();
 var ref = users.ref();
@@ -9,6 +18,30 @@ users.flush();
 users.getUserByEmail('AuthUserTesting').then(function(user) {
   console.assert(user, 'AuthUserTesting was created');
 });
+//Test writes
+var newUserRef = user.create('NewUser');
+ref.flush();
+var autoId = newPersonRef.key();
+var data = ref.getData();
+console.assert(data[autoId].first === 'NewUser', 'NewUser was created')
+
+//Test reads
+MockFirebase.override();
+people.listen();
+var greeted = [];
+people.greet = function (person) {
+  greeted.push(person);
+};
+ref.push({
+  first: 'Michael'
+});
+ref.push({
+  first: 'Ben'
+});
+ref.flush();
+console.assert(greeted.length === 2, '2 people greeted');
+console.assert(greeted[0].first === 'Michael', 'Michael greeted');
+console.assert(greeted[1].first === 'Ben', 'Ben greeted');
 
 //test test behavior that handles write errors or read errors that occur immediately
 MockFirebase.override();
