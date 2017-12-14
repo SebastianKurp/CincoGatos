d65db53e9a9c3882948a2073aedfa849b0a8b7bf
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-12-13 19:02:29 -0600

    Removed Travis, still writing test.

--- Main_Project/test/test.js
+++ Main_Project/test/test.js
@@ -1,8 +1,14 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+///canvas tests
+var expect    = require("chai").expect;
+var pbar = require("../app/progressBar");
+
+describe("Draws the progessBar", function() {
+  describe("Draws Bar", function() {
+    it("Draws", function() {
+      var bar = pbar.draw(5,600,600)
+
+      expect(bar).to.equal("360000");
+
     });
   });
 });
