286f09a3eb255cd23ceb53cb17036fee7a6bd454
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-12-13 21:04:03 -0600

    Updated a test

--- Main_Project/test/test.js
+++ Main_Project/test/test.js
@@ -1,27 +1,11 @@
-//setting up server for e2e testing
-let server
-
-before((done) => {
-  const app = express()
-  app.use('/', express.static(path.resolve(__dirname, '../../dist')))
-  server = app.listen(8080, done)
-})
-after(() => {
-  server.close()
-})
-
-const {prepareDriver, cleanupDriver} = require('../utils/browser-automation')
-
-
-describe('browser app', function () {
-  let driver
-  before(async () => {
-    driver = await prepareDriver()
-  })
-  after(() => cleanupDriver(driver))
-
-  it('should work', async function () {
-    await driver.get('http://localhost:8080')
-  }) 
-})
+MockFirebase.override();
+var ref = users.ref();
+users.create({
+  email: 'AuthUserTesting',
+  password: 'Thisisabadpassword1'
+});
+users.flush();
+users.getUserByEmail('AuthUserTesting').then(function(user) {
+  console.assert(user, 'AuthUserTesting was created');
+});
 
