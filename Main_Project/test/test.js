707179c6b0c5a44fa018663fb45c3f4e30eea90b
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-12-13 21:14:15 -0600

    Add more firebase test

--- Main_Project/test/test.js
+++ Main_Project/test/test.js
@@ -1,3 +1,4 @@
+//Test for creating a user
 MockFirebase.override();
 var ref = users.ref();
 users.create({
@@ -9,3 +10,18 @@ users.getUserByEmail('AuthUserTesting').then(function(user) {
   console.assert(user, 'AuthUserTesting was created');
 });
 
+//test test behavior that handles write errors or read errors that occur immediately
+MockFirebase.override();
+var ref = people.ref();
+var errors = [];
+log.error = function (err) {
+  errors.push(err);
+};
+people.failNext('push');
+people.create({
+  first: 'AuthUserTesting'
+});
+people.flush();
+console.assert(errors.length === 1, 'people.create error logged');
+
+//Due to difficulty canvas elements were tested manually.
